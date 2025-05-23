675086307ef0ad1579b329c86de1a4aacde8b2b2
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-23 12:41:42 -0300

    Adicionar a classe Conta com métodos de acesso e operações básicas

---
+++ src/Conta.java
@@ -0,0 +1,26 @@
+public class Conta {
+    private int numero;
+    private int agencia;
+    private double saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+    public int getAgencia() {
+        return agencia;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+        
+    }
+}
+
