17011ae67750f1dd66b72073008916212ecb0e6c
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-23 12:41:21 -0300

    Adicionar métodos getNome e setNome à classe Banco

--- src/Banco.java
+++ src/Banco.java
@@ -1,5 +1,13 @@
 public class Banco {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
+
