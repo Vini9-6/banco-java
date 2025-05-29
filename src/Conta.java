1bc1a462fcb15e5142df6c5b8d130f53e09324b8
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-29 15:09:11 -0300

    Implementar a interface IConta na classe Conta e definir métodos de saque, depósito e transferência

--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,4 @@
-public class Conta {
+public abstract class Conta implements IConta {
     private int numero;
     private int agencia;
     private double saldo;
@@ -13,14 +13,21 @@ public class Conta {
         return saldo;
     }
 
-    public void sacar(){
+    @Override
+    public void sacar(double valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sacar'");
+    }
+    @Override
+    public void depositar(double valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'depositar'");
+    }
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'transferir'");
+    }
 
-    }
-    public void depositar(){
-
-    }
-    public void transferir(){
-        
-    }
 }
 
