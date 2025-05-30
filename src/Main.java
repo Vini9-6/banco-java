85eeed1b2663d444dc2d46f4e8f16fe2a3def416
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-30 12:32:51 -0300

    Adicionar método imprimirExtrato à classe ContaPoupanca e atualizar a classe Main para exibir extratos das contas

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,22 @@ public class Main {
 
    public static void main (String[] args) {
    Banco banco = new Banco(); 
+   banco.setNome("Banco Digital");
+   System.out.println("Bem-vindo ao " + banco.getNome());
+   System.out.println("===================================");
+
    Conta cc = new ContaCorrente();
+   cc.imprimirExtrato();
+   cc.depositar(100);
+   cc.imprimirExtrato();
+
    Conta cp = new ContaPoupanca();
+   cp.imprimirExtrato();
+   cc.transferir(10, cp);
+   cc.imprimirExtrato();
+   cp.imprimirExtrato();
+   
+   cc.sacar(50);
+   cc.imprimirExtrato();
    }
 }
