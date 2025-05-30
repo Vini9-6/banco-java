e0850711b3e71c2cc49188382fcc96fad73b5808
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-30 12:32:31 -0300

    Adicionar implementação do método imprimirExtrato e refatorar imprimirInfoComum na classe Conta

--- src/Conta.java
+++ src/Conta.java
@@ -38,8 +38,14 @@ public abstract class Conta implements IConta {
     }
     @Override
     public void imprimirExtrato() {
-
+        // Método abstrato a ser implementado pelas subclasses  
     }
+    protected void imprimirInfoComum() {
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Número: " + this.numero);
+        System.out.println("Saldo: " + this.saldo);
+    }
+
 
 }
 
