2bdd8288d246b2c12b982c46abbe5e5133c159a8
Vinicius Oliveira <vengcomp@gmail.com>
2025-05-30 12:32:36 -0300

    Adicionar método imprimirExtrato à classe ContaCorrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,4 +1,7 @@
 public class ContaCorrente extends Conta {
-
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfoComum();
+    }
 }
 
